d9dd729d8282474b18ca5401df46392bfdedbd9b
Abhishek <abhishek4321u@gmail.com>
2024-03-08 13:50:13 +0530

    third

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,9 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-//load all the env variables in application
-dotenv.config();
-
 
 const url = "mongodb+srv://abhishek4321u:1234@cluster0.puuwdx6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 export const connectUsingMongoose = async()=>{
